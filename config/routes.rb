938c368da3936275f43ed2049d3531a98b1e83b6
Clebersonweng <clebersonweng@gmail.com>
2016-11-10 10:52:06 -0300

    lala

---
+++ config/routes.rb
@@ -0,0 +1,24 @@
+Rails.application.routes.draw do
+  root to: "home#index"
+
+  resources :cons_raw_materials
+  resources :cons_raw_material_details
+  resources :supplies
+  resources :program_productions
+  resources :estimate_sales
+  resources :farming_plots
+  resources :type_of_crops
+  devise_for :users
+  
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :estimate_sales do
+    resources :program_productions
+  end
+  resources  :type_of_crops do
+    resources :farming_plots
+  end
+  
+  #resources :cons_raw_materials do
+   # resources :cons_raw_material_details
+ # end
+end
