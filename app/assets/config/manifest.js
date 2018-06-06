daf8905dc95414e0298c53084d67bdfac9928e9a
Clebersonweng <clebersonweng@gmail.com>
2018-06-05 22:14:40 -0400

    css

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -1,10 +1,10 @@
-//= link_tree ../images
-//= link_directory ../assets/javascripts .js
-//= link_directory ../assets/stylesheets .css
-//= link_directory  ../assets/fonts      .otf
-//= link_directory  ../assets/fonts      .svg
-//= link_directory  ../assets/fonts      .ttf
-//= link_directory  ../assets/fonts      .woff
-//= link_directory  ../assets/fonts      .woff2
+//= link_tree /images
+//= link_directory /assets/javascripts .js
+//= link_directory /assets/stylesheets .css
+//= link_directory  /assets/fonts      .otf
+//= link_directory  /assets/fonts      .svg
+//= link_directory  /assets/fonts      .ttf
+//= link_directory  /assets/fonts      .woff
+//= link_directory  /assets/fonts      .woff2
 
 
