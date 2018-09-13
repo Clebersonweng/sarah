92c51017ea1ae93ab8c1ab6a08b6a7d5708178af
Clebersonweng <clebersonweng@gmail.com>
2018-09-13 00:48:17 -0400

    creado crud de coef de revaluo

--- app/controllers/revaluation_coefficients_controller.rb
+++ app/controllers/revaluation_coefficients_controller.rb
@@ -1,47 +1,56 @@
 class RevaluationCoefficientsController < ApplicationController
-  before_action :set_revaluation_coefficient, only: [:show, :edit, :update, :destroy]
+   before_action :set_revaluation_coefficient, only: [:show, :edit, :update, :destroy]
 
-  respond_to :html
+   respond_to :html
 
-  def index
-    @revaluation_coefficients = RevaluationCoefficient.all
-    respond_with(@revaluation_coefficients)
-  end
+   def index
+      get_all
+      @revaluation_coefficients = RevaluationCoefficient.all
+      respond_with(@revaluation_coefficients)
+   end
 
-  def show
-    respond_with(@revaluation_coefficient)
-  end
+   def show
+      respond_with(@revaluation_coefficient)
+   end
 
-  def new
-    @revaluation_coefficient = RevaluationCoefficient.new
-    respond_with(@revaluation_coefficient)
-  end
+   def new
+      get_all
+      @revaluation_coefficient = RevaluationCoefficient.new
+      respond_with(@revaluation_coefficient)
+   end
 
-  def edit
-  end
+   def edit
+      get_all
+   end
 
-  def create
-    @revaluation_coefficient = RevaluationCoefficient.new(revaluation_coefficient_params)
-    @revaluation_coefficient.save
-    respond_with(@revaluation_coefficient)
-  end
+   def create
+      @revaluation_coefficient = RevaluationCoefficient.new(revaluation_coefficient_params)
+      @revaluation_coefficient.save
+      respond_with(@revaluation_coefficient)
+   end
 
-  def update
-    @revaluation_coefficient.update(revaluation_coefficient_params)
-    respond_with(@revaluation_coefficient)
-  end
+   def update
+      @revaluation_coefficient.update(revaluation_coefficient_params)
+      respond_with(@revaluation_coefficient)
+   end
 
-  def destroy
-    @revaluation_coefficient.destroy
-    respond_with(@revaluation_coefficient)
-  end
+   def destroy
+      @revaluation_coefficient.destroy
+      respond_with(@revaluation_coefficient)
+   end
 
-  private
-    def set_revaluation_coefficient
+   private
+   def set_revaluation_coefficient
       @revaluation_coefficient = RevaluationCoefficient.find(params[:id])
-    end
+   end
 
-    def revaluation_coefficient_params
+   def revaluation_coefficient_params
       params.require(:revaluation_coefficient).permit(:start_period, :end_period, :coefficient)
-    end
+   end
+
+   def get_all
+      @path = " / configuraciones / coeficiente de revalúo"
+      @edit_name      = "Editar coeficiente de revalúo"
+   end
+
 end
